16aae67ef4d0f50f894814ef3c0adf8602b12557
Tom O'Dwyer <58118455+Tatooine-Soldier@users.noreply.github.com>
2022-07-30 20:14:14 +0100

    Add files via upload
    Using LCD with IR remote reciever

---
+++ sketch_jul28a.ino
@@ -0,0 +1,32 @@
+#include <LiquidCrystal.h>
+#include <IRremote.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int RECV_PIN = 10;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+  irrecv.enableIRIn();
+  pinMode(RECV_PIN, INPUT);
+  
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+  if (irrecv.decode(&results)){
+        Serial.println(results.value, HEX);
+        irrecv.resume();
+  }
+}
